49759586c7c86d804e051dd71ba51424e36e1043
Cem Ünalan <unalancem@gmail.com>
2018-03-04 15:34:13 +0300

    Added comments to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 
 const actions = require('./actions.js');
 
+// Takes out first two console arguments
 const processArgs = process.argv.slice(2);
 
+// if there are no arguments we assume the user want to generate tc id number
 if (processArgs[0] === undefined) {
   actions.generate();
 }
 
+// table that maps arguments to commands
 const commandsForArguments = {
   '-d': actions.validate,
   '--dogrula': actions.validate,
@@ -16,11 +19,14 @@ const commandsForArguments = {
   '--yardim': actions.help,
 };
 
+// get action to run
 const toRun = commandsForArguments[processArgs[0]];
 
+// if its invalid action run help action
 if (toRun === undefined) {
   console.log('Lütfen geçerli bir komut giriniz');
   actions.help();
 }
 
+// run the action
 toRun();
